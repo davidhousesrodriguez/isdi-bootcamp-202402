4803d3914db7cf01789274c670a84c4c452a4b10
davidhousesrodriguez <davidhouses@hotmail.com>
2024-03-03 19:05:59 +0100

    add find,indexOf, filter Arroz prototypes#387

--- staff/david-casas/prototypes/arroz/Arroz.js
+++ staff/david-casas/prototypes/arroz/Arroz.js
@@ -1,13 +1,13 @@
 function Arroz() {
-    if(arguments.length !== 1) {
+    if (arguments.length !== 1) {
         this.length = arguments.length
 
-        for(var i = 0; i < arguments.length; i++) {
-          var argument = arguments[i]
-          
-          this[i] = argument
+        for (var i = 0; i < arguments.length; i++) {
+            var argument = arguments[i]
+
+            this[i] = argument
         }
-    }else {
+    } else {
         var argument = arguments[0]
 
         if (typeof argument === 'number') {
@@ -28,13 +28,11 @@ Arroz.prototype.push = function () {
         this[this.length] = argument
         this.length++
     }
-
     return this.length
 }
 
 Arroz.prototype.pop = function () {
-    var lastIndex = this.length -1
-
+    var lastIndex = this.length - 1
     var last = this[lastIndex]
 
     delete this[lastIndex]
@@ -45,37 +43,27 @@ Arroz.prototype.pop = function () {
 }
 
 Arroz.prototype.toString = function () {
-    var string= 'Arroz ['
+    var string = 'Arroz ['
 
     for (var i = 0; i < this.length; i++) {
         var element = this[i]
 
         string += element
 
-        if( i < this.length - 1)
-        string += ', '
+        if (i < this.length - 1) {
+            string += ', '
+        }
     }
-    
+
     string += ']'
 
     return string
 }
-/*
-Arroz.prototype.some = function() {
-    
-    for(var i = 0; i < this.length; i++) {
-        
-       if(this[i] === true) {
-        return true
-       }
-       
-    }
-    return false
+
+Arroz.prototype.at = function (index) {
+    return this[index > -1 ? index : index + this.length]
 }
-*/
-Arroz.prototype.at = function(index) {
-    return this[ index > -1 ? index: index +this.length]
-    }
+
 Arroz.prototype.concat = function (added) {
     if (added instanceof Array) {
         for (var i = 0; i < added.length; i++) {
@@ -88,16 +76,125 @@ Arroz.prototype.concat = function (added) {
 
     return this
 }
-Arroz.prototype.every = function(callback) {
+
+Arroz.prototype.every = function (callback) {
     for (var i = 0; i < this.length; i++) {
         var element = this[i]
-        if(element !== undefined) {
-            if (callback(elemnent) === false)
-            return false
+        if (element !== undefined) {
+            if (callback(element) === false)
+                return false
         }
     }
     return true
 }
 
+Arroz.prototype.includes = function (callback) {
+    for (var i = 0; i < this.length; i++) {
+        var element = this[i]
+        if (element !== undefined) {
+            if (callback(element) === true)
+                return true
+        }
+    }
+    return false
+}
 
+Arroz.prototype.indexOf = function (value, index) {
+    if (arguments.length === 1) {
+        for (var i = 0; i < this.length; i++) {
+            element = this[i]
+            if (element === value) {
+                return i
+            }
+        }
+    } else if (arguments.length === 2) {
+        index = index > -1 ? index : index + this.length - 1
+        if (index < 0 || index > this.length) {
+            return -1
+        }
+        for (var i = index; i < this.length; i++) {
+            element = this[i]
+            if (element === value) {
+                return i
+            }
+        }
+    }
+}
+
+Arroz.prototype.shift = function () {
+    var firstIndex = 0
+
+    var first = this[firstIndex]
+
+    delete this[firstIndex]
+
+    this.lengt--
+
+    return first
+}
+
+Arroz.prototype.filter = function(callback) {
+    var reArroz = new Arroz()
+    for (var i = 0; i < this.length; i++) {
+        if(callback(this[i]) === true)
+            reArroz.push(this[i])
+    }
+    return reArroz
+ }
+
+ Arroz.prototype.find = function(callback) {
+    var result = undefined
+    for(var i = 0; i < this.length; i++) {
+        if(callback(this[i]) === true) {
+           result =this[i]
+           return result     
+        }
+    }
+    return result
+ }
+
+ Arroz.prototype.findIndex = function(callback) {
+    var result = -1
+    for(var i = 0; i < this.length; i++) {
+        if(callback(this[i]) === true) {
+        result = i
+        return result
+        }
+    }
+    return result
+}
+
+Arroz.prototype.includes = function(){
+    if (arguments.length === 1){
+        for (var i = 0; i < this.length; i++){
+            if (this[i] === arguments[0])
+            return true
+        }
+    } else {
+        for (var i = arguments[1]; i < this.length; i++){
+            if (this[i] === arguments[0])
+            return true
+        }
+    }    
+    return false
+}
+Arroz.prototype.forEach = function(callback){
+    for(let i = 0; i < this.length; i++){
+        callback(this[i])
+    }
+}
+   
+
+/*
+Arroz.prototype.unshift = function (callback) {
+   var firstIndex = 0
+   var added = this[firstIndex]
+
+    this.length++
+
+    return added
+   
+   
+}
+*/
 module.exports = Arroz
--- staff/david-casas/prototypes/arroz/Arroz.spec.js
+++ staff/david-casas/prototypes/arroz/Arroz.spec.js
@@ -3,9 +3,8 @@ var matcha = require('./matcha')
 var Arroz = require('./Arroz')
 
 matcha.describe('Arroz', function () {
-    matcha.describe('> constructor', function () {
-        matcha.it('should construct', function () 
-        {
+    matcha.describe('> construct', function () {
+        matcha.it('should construct', function () {
             var a = new Arroz
 
             matcha.expect(a).toBeInstanceOf(Arroz)
@@ -16,7 +15,7 @@ matcha.describe('Arroz', function () {
             var a = new Arroz(10, 20, 30)
 
             matcha.expect(a).toBeInstanceOf(Arroz)
-            matcha.expect(a.length).toBe (3)
+            matcha.expect(a.length).toBe(3)
             matcha.expect(a[0]).toBe(10)
             matcha.expect(a[1]).toBe(20)
             matcha.expect(a[2]).toBe(30)
@@ -30,97 +29,71 @@ matcha.describe('Arroz', function () {
             matcha.expect(a[0]).toBe(true)
         })
 
-        matcha.it('should construct with one numeric value', function () {
+        matcha.it('should construct with ONE NUMERIC value', function () {
             var a = new Arroz(5)
 
             matcha.expect(a).toBeInstanceOf(Arroz)
+
             matcha.expect(a.length).toBe(5)
-            for(var i = 0; i < a.length; i++)
-            matcha.expect(a[i]).toBe(undefined)
+            for (var i = 0; i < a.length; i++) {
+                matcha.expect(a[i]).toBe(undefined)
+            }
         })
-    })    
-        matcha.describe('> push', function () {
-            matcha.it('should push a value', function () {
-                var a = new Arroz(10, 20, 30)
-
-                matcha.expect(!!a.push). toBe(true)
-
-                var length= a.push(40)
-
-                matcha.expect(a.length).toBe(4)
-                matcha.expect(a[a.length - 1]).toBe (40)
-                matcha.expect(length).toBe(4)
-            })
-
-            matcha.it('should push many values', function () {
-                var a = new Arroz(10, 20, 30)
-
-                matcha.expect(!!a.push).toBe(true)
-
-                var length = a.push(40, 50, 60, 70)
-
-                matcha.expect(a.length).toBe(7)
-                matcha.expect(a[3]).toBe(40)
-                matcha.expect(a[4]).toBe(50)
-                matcha.expect(a[5]).toBe(60)
-                matcha.expect(a[6]).toBe(70)
-                matcha.expect(length).toBe(7)
-            })
-        })
-
-        matcha.describe('>pop', function () {
-            matcha.it('should extract last value', function () {
-                var a = new Arroz(10, 20, 30)
-
-                matcha.expect(!!a.pop).toBe(true)
-
-                var value = a.pop()
-
-                matcha.expect(a.length).toBe(2)
-                matcha.expect(a[0]).toBe (10)
-                matcha.expect(a[1]).toBe (20)
-                matcha.expect(a[2]).toBe(undefined)
-                matcha.expect(value).toBe(30)
-            })
-         })
-
-         matcha.describe('> toString', function () {
-            matcha.it('should convert to string', function () {
-                var a = new Arroz(10, 20, 30, 40, 50)
-
-                matcha.expect(!!a.toString).toBe(true)
-
-                var string = a.toString()
-
-                matcha.expect(string).toBe('Arroz [10, 20, 30, 40, 50]')
-            })
-        })
-        /*//no funciona
-        matcha.describe ('>some', function () {
-            matcha.it ('should return true if element is in the array', function() {
-                  
-          var a = new Arroz(10, 20, 30)
-        
-          matcha.expect(!!a.some).toBe(true)
-
-          var value = a.some()
-          
-          matcha.expect(a.length).toBe(3)
-          matcha.expect(a[0]).toBe(10)
-          matcha.expect(a[1]).toBe(20)
-          matcha.expect(a[2]).toBe(30)
-          matcha.expect(value).toBe( )
-        })
-
     })
-*/
+
+    matcha.describe('> push', function () {
+        matcha.it('should push a value', function () {
+            var a = new Arroz(10, 20, 30)
+
+            matcha.expect(!!a.push).toBe(true)
+
+            var length = a.push(40, 50, 60, 70)
+
+            matcha.expect(a.length).toBe(7)
+            matcha.expect(a[3]).toBe(40)
+            matcha.expect(a[4]).toBe(50)
+            matcha.expect(a[5]).toBe(60)
+            matcha.expect(a[6]).toBe(70)
+            matcha.expect(length).toBe(7)
+        })
+    })
+
+    matcha.describe('> pop', function () {
+        matcha.it('should extract last value', function () {
+            var a = new Arroz(10, 20, 30)
+
+            matcha.expect(!!a.pop).toBe(true)
+
+            var value = a.pop()
+
+            matcha.expect(a.length).toBe(2)
+            matcha.expect(a[0]).toBe(10)
+            matcha.expect(a[1]).toBe(20)
+            matcha.expect(a[2]).toBe(undefined)
+            matcha.expect(value).toBe(30)
+        })
+    })
+
+    matcha.describe('> toString', function () {
+        matcha.it('should convert to string', function () {
+            var a = new Arroz(10, 20, 30, 40)
+
+            matcha.expect(!!a.toString).toBe(true)
+
+            var string = a.toString()
+
+            matcha.expect(string).toBe('Arroz [10, 20, 30, 40]')
+        })
+    })
+
     matcha.describe('> at', function () {
-        matcha.it('should return position of array(positive index', function () {
+        matcha.it('should return position of Arroz (positive index)', function () {
             var a = new Arroz(10, 20, 30, 40)
 
             matcha.expect(!!a.at).toBe(true)
 
             var atValue = a.at(2)
+
             matcha.expect(atValue).toBe(30)
 
             matcha.expect(a[0]).toBe(10)
@@ -128,15 +101,14 @@ matcha.describe('Arroz', function () {
             matcha.expect(a[2]).toBe(30)
             matcha.expect(a[3]).toBe(40)
             matcha.expect(a[4]).toBe(undefined)
-
         })
 
-        matcha.it('should return position of Arroz(negative index', function () {
+        matcha.it('should return position of Arroz (negative index)', function () {
 
             var a = new Arroz(10, 20, 30, 40)
-            var negValue = a.at(-2)
+            var atValueMinus = a.at(-2)
 
-            matcha.expect(negValue).toBe(30)
+            matcha.expect(atValueMinus).toBe(30)
 
             matcha.expect(a[0]).toBe(10)
             matcha.expect(a[1]).toBe(20)
@@ -145,10 +117,12 @@ matcha.describe('Arroz', function () {
             matcha.expect(a[4]).toBe(undefined)
         })
 
-        matcha.it('should return position of Arroz (OOR index)', function () {
+        matcha.it('should return position of Arroz (OUT OF RANGE index)', function () {
 
             var a = new Arroz(10, 20, 30, 40)
-            var negValue = a.at(-70)
+            var atValueMinus = a.at(-70)
+
+            matcha.expect(undefined).toBe(undefined)
 
             matcha.expect(a[0]).toBe(10)
             matcha.expect(a[1]).toBe(20)
@@ -156,12 +130,13 @@ matcha.describe('Arroz', function () {
             matcha.expect(a[3]).toBe(40)
             matcha.expect(a[4]).toBe(undefined)
         })
-    }) 
-    
-    matcha.describe('>concat', function () {
-        matcha.it('should return concatenation of single value(string/number)', function () {
+    })
+
+    matcha.describe('> concat', function () {
+        matcha.it('should return concatenation of single value (string/number)', function () {
             var a = new Arroz(10, 20, 30)
-            matcha.expect(!! a.concat).toBe(true)
+
+            matcha.expect(!!a.concat).toBe(true)
 
             a.concat('40')
 
@@ -180,6 +155,8 @@ matcha.describe('Arroz', function () {
             matcha.expect(a[2]).toBe(30)
             matcha.expect(a[3]).toBe(40)
             matcha.expect(a[4]).toBe(undefined)
+
+
         })
 
         matcha.it('should return concatenation of arrays', function () {
@@ -195,6 +172,7 @@ matcha.describe('Arroz', function () {
 
         })
     })
+
     matcha.describe('> every', function () {
         matcha.it('should return true if all elements meet condition HAPPY', function () {
             var a = new Arroz(10, 20, 30)
@@ -203,19 +181,6 @@ matcha.describe('Arroz', function () {
             var result = a.every(function (x) {
                 return x < 40
             })
-            matcha.expect(result).toBe(true)
-
-            matcha.expect(a[0]).toBe(10)
-            matcha.expect(a[1]).toBe(20)
-            matcha.expect(a[2]).toBe(30)
-        })
-
-        matcha.it('should return true if all elemensts meet condition UNHAPPY', function () {
-            var a = new Arroz(10, 20, 30)
-
-            var result = a.every(function (x) {
-                return x < 20
-            })
 
             matcha.expect(result).toBe(true)
 
@@ -223,7 +188,8 @@ matcha.describe('Arroz', function () {
             matcha.expect(a[1]).toBe(20)
             matcha.expect(a[2]).toBe(30)
         })
-        matcha.it('should return true if all elements meet condition UNHAPPY', function () {
+
+        matcha.it('should return false if all elements meet condition UNHAPPY', function () {
             var a = new Arroz(10, 20, 30)
 
             var result = a.every(function (x) {
@@ -237,4 +203,219 @@ matcha.describe('Arroz', function () {
             matcha.expect(a[2]).toBe(30)
         })
     })
+    matcha.describe('> includes', function () {
+        matcha.it('should return true if at least 1 element meets condition HAPPY', function () {
+            var a = new Arroz(10, 20, 30)
+            matcha.expect(!!a.includes).toBe(true)
+
+            var result = a.includes(function (x) {
+                return x < 20
+            })
+
+            matcha.expect(result).toBe(true)
+
+            matcha.expect(a[0]).toBe(10)
+            matcha.expect(a[1]).toBe(20)
+            matcha.expect(a[2]).toBe(30)
+        })
+
+        matcha.it('should return false if at least 1 element meets condition UNHAPPY', function () {
+            var a = new Arroz(10, 20, 30)
+
+            var result = a.includes(function (x) {
+                return x < 5
+            })
+
+            matcha.expect(result).toBe(false)
+
+            matcha.expect(a[0]).toBe(10)
+            matcha.expect(a[1]).toBe(20)
+            matcha.expect(a[2]).toBe(30)
+        })
+    })
+
+    matcha.describe('> indexOf', function () {
+        matcha.it('should return index of item nearer to index 0', function () {
+            var a = new Arroz(10, 30, 20, 30)
+            matcha.expect(!!a.indexOf).toBe(true)
+
+            var result = a.indexOf(30)
+
+            matcha.expect(result).toBe(1)
+        })
+
+        matcha.it('should return index of item nearer to index that I input', function () {
+            var a = new Arroz(10, 30, 20, 30)
+
+            var result = a.indexOf(30, 1)
+
+            matcha.expect(result).toBe(1)
+
+
+            var a = new Arroz(10, 30, 20, 30)
+
+            var result = a.indexOf(30, -1)
+
+            matcha.expect(result).toBe(3)
+        })
+
+        matcha.it('should return -1 if input index is out of bounds UNHAPPY', function () {
+            var a = new Arroz(10, 30, 20, 30)
+
+            var result = a.indexOf(30, 7)
+
+            matcha.expect(result).toBe(-1)
+
+
+            var a = new Arroz(10, 30, 20, 30)
+
+            var result = a.indexOf(30, -7)
+
+            matcha.expect(result).toBe(-1)
+        })
+    })
+    
+    matcha.describe('> shift' , function () {
+        matcha.it('should remove the first element', function() {
+            var a = new Arroz (10, 20, 30, 40)
+
+            matcha.expect(!!a.shift).toBe(true)
+
+            var first = a.shift()
+
+            matcha.expect(a.length).toBe(4)
+            matcha.expect(a[0]).toBe(undefined)
+            matcha.expect(a[1]).toBe(20)
+            matcha.expect(a[2]).toBe(30)
+            matcha.expect(a[3]).toBe(40)
+            matcha.expect(first).toBe(10)
+        })
+    })
+
+    matcha.describe('> filter', function(){
+        matcha.it('should return Arroz with every matching element', function(){
+            var newArroz = new Arroz(10, 20, 30)
+
+            matcha.expect(!!newArroz.filter).toBe(true)
+
+            var result = newArroz.filter(function (x){
+                return x > 10
+            })
+
+            matcha.expect(result[0]).toBe(20)
+            matcha.expect(result[1]).toBe(30)
+            matcha.expect(result.length).toBe(2)
+        })
+
+        matcha.it('should return empty Arroz on no matches', function(){
+            var newArroz = new Arroz(10, 20, 30)
+
+            var result = newArroz.filter(function (x){
+                return x > 30
+            })
+
+            matcha.expect(result.length).toBe(0)
+        })
+    })
+    matcha.describe('find', function() {
+        matcha.it('should return the first element in the Arroz that satisfies the given function', function () {
+            var newArr = new Arroz (10, 20, 30)
+
+            matcha.expect(!!newArr.find).toBe(true)
+
+            var result = newArr.find(function (x) { return x > 11
+            })
+            matcha.expect(result).toBe(20)
+        })
+        matcha.it('should return undefined when no matches', function() {
+            var newArr = new Arroz(10, 20, 30)
+
+            var result = newArr.find(function (x) {
+                return x > 30
+            })
+            matcha.expect(result).toBe(undefined)
+        })
+    })
+
+    matcha.describe('>findIndex', function() {
+        matcha.it('should return the first index in the Arroz wich satisfy the given function', function() {
+            var newArr = new Arroz(10, 20, 30)
+
+            matcha.expect(!!newArr.findIndex).toBe(true)
+
+            var result = newArr.findIndex(function(x) {return x > 9
+            })
+        
+        matcha.expect(result).toBe(0)
+    })
+        matcha.it('should return -1 if no matches', function() {
+            var newArr = new Arroz(10, 20, 30)
+            var result = newArr.findIndex(function(x){ return x > 30
+
+            })
+            matcha.expect(result).toBe(-1)
+        })    
+    })
+
+    matcha.describe('>includes', function() {
+        matcha.it('should return true if Arroz entry in in Arroz according to test', function() {
+            var newArr = new Arroz(10, 20, 30)
+
+            matcha.expect(!!newArr.includes).toBe(true)
+            
+            var result = newArr.includes(20)
+            matcha.expect(result).toBe(true)
+        })
+        matcha.it('should return false if Arroz isnt into one of entries', function () {
+            var newArra = new Arroz(10, 20, 30)
+            var result = newArra.includes(40)
+            matcha.expect(result).toBe(false)
+        })
+        matcha.it('should start from index if given one', function(){
+            var newArroz = new Arroz(10, 20, 30)
+            var result = newArroz.includes(20, 1)
+            matcha.expect(result).toBe(true)
+    })
+})
+    matcha.describe('>forEach', function () {
+        matcha.it('should execute a provided function once for each element of Arroz')
+        var result = new Arroz(10, 20, 30)
+        var resultArroz = new Arroz()
+
+        matcha.expect(!!result).toBe(true)
+        result.forEach(function (x) {
+            resultArroz.push(x * 2)
+        })
+        matcha.expect(resultArroz.length).toBe(3)
+        matcha.expect(resultArroz[0]).toBe(20)
+        matcha.expect(resultArroz[1]).toBe(40)
+        matcha.expect(resultArroz[2]).toBe(60)
+})
+
+    
+/*
+    
+    matcha.describe('>unshift', function () {
+        matcha.it('should add specified elements at the begining of an array', function () {
+            var a = new Arroz (10, 20, 30, 40)
+
+            matcha.expect(!!a.unshift).toBe(true)
+
+            var result= a.shift(1, 5, 9)
+
+            matcha.expect(result.length). toBe(7)
+            matcha.expect(result[0]).toBe(1)
+            matcha.expect(result[1]).toBe(5)
+            matcha.expect(result[2]).toBe(9)
+            matcha.expect(result[3]).toBe(10)
+            matcha.expect(result[4]).toBe(20)
+            matcha.expect(result[5]).toBe(30)
+            matcha.expect(result[6]).toBe(40)
+            matcha.expect(result).toBe(0, 5, 9, 10, 20, 30, 40)
+            
+        })
+    })
+  
+})
+*/
 })
