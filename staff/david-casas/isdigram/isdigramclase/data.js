ceb3f0f4e55114032ad79dacaac72dbc698fc38c
davidhousesrodriguez <davidhouses@hotmail.com>
2024-03-08 19:10:42 +0100

    isdigramclase updated #325

--- staff/david-casas/isdigram/isdigramclase/data.js
+++ staff/david-casas/isdigram/isdigramclase/data.js
@@ -1,4 +1,129 @@
-// data
+// data layer
 
-var users = localStorage.users ?
-JSON.parse(localStorage.users) : []
+var data = (function () {
+    // helper
+
+    function generateId() {
+        return (+((parseInt(Math.random() * 10 ** 17)).toString())).toString(36)
+    }
+
+    function loadUsers() {
+        return JSON.parse(localStorage.users || '[]')
+    }
+
+    function loadPosts() {
+        return JSON.parse(localStorage.posts || '[]')
+    }
+
+    function saveUsers(users) {
+        localStorage.users = JSON.stringify(users)
+    }
+
+    function savePosts(posts) {
+        localStorage.posts = JSON.stringify(posts)
+    }
+
+    // data
+
+    function findUser(callback) {
+        var users = loadUsers()
+
+        var user = users.find(callback)
+
+        return user
+    }
+
+    function insertUser(user) {
+        var users = loadUsers()
+
+        user.id = generateId()
+
+        users.push(user)
+
+        saveUsers(users)
+    }
+
+    function printUsers() {
+        var users = loadUsers()
+
+        console.table(users)
+    }
+
+    function updateUser(user) {
+        var users = loadUsers()
+
+        var index = users.findIndex(function (user2) {
+            return user2.id === user.id
+        })
+
+        if (index > - 1) {
+            users.splice(index, 1, user)
+
+            saveUsers(users)
+        }
+    }
+
+    function getAllUsers() {
+        var users = loadUsers()
+
+        return users
+    }
+
+    function insertPost(post) {
+        var posts = loadPosts()
+
+        post.id = generateId()
+
+        posts.push(post)
+
+        savePosts(posts)
+    }
+
+    function getAllPosts() {
+        var posts = loadPosts()
+
+        return posts
+    }
+
+    function findPost(callback) {
+        var posts = loadPosts()
+
+        var post = posts.find(callback)
+
+        return post
+    }
+
+    function toChangePost (callback) {
+        var posts = loadPosts()
+
+        var index = posts.findIndex(callback)
+
+        posts[index] = 'pepe' 
+
+        savePosts(posts)
+    }
+
+    function deletePost(callback) {
+        var posts = loadPosts()
+
+        var index = posts.findIndex(callback)
+
+        posts.splice(index, 1)
+
+        savePosts(posts)
+    }
+
+    return {
+        findUser: findUser,
+        insertUser: insertUser,
+        printUsers: printUsers,
+        updateUser: updateUser,
+        getAllUsers: getAllUsers,
+        insertPost: insertPost,
+        getAllPosts: getAllPosts,
+        findPost: findPost,
+        editPost: editPost,
+        toCHangePost:toChangePost,
+        deletePost: deletePost
+    }
+})()
--- staff/david-casas/isdigram/isdigramclase/utils.js
+++
@@ -1,7 +0,0 @@
-//utils
-
-function block (millis) {
-    var before= Date.now()
-    while (Date.now() - before < millis);
-
-}
