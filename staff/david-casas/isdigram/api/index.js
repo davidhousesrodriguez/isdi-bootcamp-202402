0bfaf8e96c21ff1dea3b786c80a4dc98e401717c
davidhousesrodriguez <davidhouses@hotmail.com>
2024-03-25 19:21:19 +0100

    implement Componetization and React versions #325

---
+++ staff/david-casas/isdigram/api/index.js
@@ -0,0 +1,20 @@
+const fs = require("fs");
+
+console.log("start");
+
+try {
+  fs.readFile("./users.json", "utf8", (error, usersJson) => {
+    if (error) {
+      console.log(error);
+
+      return;
+    }
+    console.log(usersJson);
+
+    console.log("end");
+  });
+} catch (error) {
+  console.error(error);
+}
+
+console.log("continue...");
